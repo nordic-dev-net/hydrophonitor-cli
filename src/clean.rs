3f099cdc68c6c0cd281abb9e666603ddacac7fe1
Max Winter <maxwinter00@gmail.com>
2023-11-13 16:28:39 +0100

    added clean command dummy implementation
    Signed-off-by: Max Winter <maxwinter00@gmail.com>

---
+++ src/clean.rs
@@ -0,0 +1,21 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(about = "This command removes all deployment data from the given device's /output path")]
+pub struct Clean {
+    ///Path to USB mass storage or SD card where data will be deleted from.
+    #[clap(short, long, required = True)]
+    pub device: PathBuf,
+
+    ///Increases the CLI verbosity.
+    #[clap(short, long, action)]
+    pub verbose: bool,
+}
+
+impl Clean {
+    pub fn clean(&self) {
+        println!("Cleaning device at {:?}", self.device);
+    }
+}
