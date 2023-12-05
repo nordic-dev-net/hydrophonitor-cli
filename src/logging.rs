9b8290a4373afe57116f3e86293e4b179217b4b2
Max Winter <maxwinter00@gmail.com>
2023-12-05 14:46:46 +0100

    implemented error message for invalid verbose level
    Signed-off-by: Max Winter <maxwinter00@gmail.com>

--- src/logging.rs
+++ src/logging.rs
@@ -1,15 +1,23 @@
-use log::{info, LevelFilter};
+use log::{error, info, LevelFilter};
 
 pub fn init_logging(verbose: &String) {
+    let mut invalid_verbosity = false;
     let level = match verbose.as_str() {
         "error" => LevelFilter::Error,
         "warn" => LevelFilter::Warn,
         "info" => LevelFilter::Info,
         "debug" => LevelFilter::Debug,
         "trace" => LevelFilter::Trace,
-        _ => LevelFilter::Error //TODO throw an error
+        _ => {
+            invalid_verbosity = true;
+            LevelFilter::Error
+        }
     };
     env_logger::builder().filter_level(level).init();
-    info!("Initialized logging with log level {}", level)
+    if invalid_verbosity {
+        error!("Invalid verbosity {verbose}! Using log level 'error' as fallback!")
+    } else {
+        info!("Initialized logging with log level {}", level)
+    }
 }
 
