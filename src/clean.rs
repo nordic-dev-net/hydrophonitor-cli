b039b4662e6e4b4f7a05d5de89762a65ac16507a
Max Winter <maxwinter00@gmail.com>
2023-12-07 17:27:56 +0100

    added question if the user really wants to delete the entries
    Signed-off-by: Max Winter <maxwinter00@gmail.com>

--- src/clean.rs
+++ src/clean.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, io};
 use std::path::PathBuf;
 
 use clap::Parser;
@@ -13,24 +13,56 @@ pub struct Clean {
 }
 
 impl Clean {
-    pub fn clean(&self) {
+    pub fn clean(&mut self) {
         info!("Cleaning device at {:?}", &self.device);
-        let mut output_folder = self.device.clone();
-        output_folder.push("output");
+        self.device.push("output");
 
-        match fs::remove_dir_all(&output_folder) {
+        // Attempt to open the directory
+        match fs::read_dir(&self.device) {
+            Ok(entries) => {
+                // Iterate over the entries in the directory
+                let mut entries_found = false;
+                for entry in entries {
+                    if let Ok(entry) = entry {
+                        // Print the name of each entry
+                        println!("{}", entry.file_name().to_string_lossy());
+                        entries_found = true;
+                    }
+                }
+                if entries_found {
+                    println!("Do you really want to delete these entries? (y/n)");
+                    let mut user_input = String::new();
+                    io::stdin().read_line(&mut user_input).expect("Failed to read line");
+                    if !(user_input.contains("y") || user_input.contains("Y")) {
+                        println!("Aborting!");
+                        return;
+                    }
+                } else {
+                    println!("The directory is already empty!");
+                    return;
+                }
+            }
+            Err(e) => {
+                error!("Error opening the directory {:?}: {}", &self.device, e);
+                return;
+            }
+        }
+
+        match fs::remove_dir_all(&self.device) {
             Ok(_) => {}
             Err(e) => {
-                error!("Removing everything in directory {:?} failed: {}", &output_folder, e);
+                error!("Removing everything in directory {:?} failed: {}", &self.device, e);
                 return;
             }
         };
-        match fs::create_dir(output_folder) {
+        match fs::create_dir(&self.device) {
             Ok(_) => {}
             Err(e) => {
                 error!("Creating new output directory in {:?} failed: {}", &self.device, e);
                 return;
             }
         };
+
+        println!("Successfully cleaned directory!")
     }
 }
