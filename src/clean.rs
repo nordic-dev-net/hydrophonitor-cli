c74756e64442eadf1aaebfd1e15e254761f49dd1
Max Winter <maxwinter00@gmail.com>
2023-12-07 16:14:39 +0100

    reading entries in output directory
    Signed-off-by: Max Winter <maxwinter00@gmail.com>

--- src/clean.rs
+++ src/clean.rs
@@ -1,18 +1,35 @@
 use std::path::PathBuf;
 
 use clap::Parser;
-use log::info;
+use log::{error, info};
 
 #[derive(Parser, Debug)]
 #[clap(about = "This command removes all deployment data from the given device's /output path")]
 pub struct Clean {
     ///Path to USB mass storage or SD card where data will be deleted from.
     #[clap(short, long, required = true)]
-    pub device: PathBuf,
+    device: PathBuf,
 }
 
 impl Clean {
-    pub fn clean(&self) {
-        info!("Cleaning device at {:?}", self.device);
+    pub fn clean(&mut self) {
+        info!("Cleaning device at {:?}", &self.device);
+        self.device.push("output");
+
+        // Attempt to open the directory
+        match self.device.read_dir() {
+            Ok(entries) => {
+                // Iterate over the entries in the directory
+                for entry in entries {
+                    if let Ok(entry) = entry {
+                        // Print the name of each entry
+                        println!("{}", entry.file_name().to_string_lossy());
+                    }
+                }
+            }
+            Err(e) => {
+                error!("Error opening the output folder of the device at {:?}: {}", &self.device, e);
+            }
+        }
     }
 }
